083d51329d044aeaea47326a881fcc84eb40d2a0
SebWitt <sewitt@mailbox.org>
2016-01-06 15:39:03 +0100

    filter mit scipy

--- sk.py
+++ sk.py
@@ -3,6 +3,7 @@ import skimage.data
 import skimage.io as io
 import skimage.filters as filters
 import skimage.util as util
+import scipy.ndimage as ndimage
 
 
 img_stack = []
@@ -16,6 +17,9 @@ def read_img(pic):
 def show_img():
     global img
     io.imshow(img, "qt")
+    img = ndimage.gaussian_filter(img, 1)
+    io.imshow(img, "qt")
+    
 
 def refresh_img():
     pass
