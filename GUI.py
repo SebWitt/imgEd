e72253ce59b5fac961639008fc4b881e5d67a732
gunnumu <hawaiiquestionmark@web.de>
2016-01-15 23:17:10 +0100

    to scale % doesnt work, scipy just cant handle it. rest works fine at my place

--- GUI.py
+++ GUI.py
@@ -6,10 +6,11 @@ from PIL import ImageTk
 import sk
 import pseudoStack
 import tempfile
-from numpy import save
-
-#pseudoStack = [] # <-- Stack!&%"&$"!"§$
-
+import numpy as np
+from matplotlib import pyplot as plt
+from skimage import io
+"""This module implements a rudimentary image processing tool."""
+###############################################################################
 
 # not DAU approved, so care about input
 # works best with *.jpg
@@ -18,91 +19,155 @@ from numpy import save
 #   can't figure out why
 class Toolbar():
     
+    """
+    
+    A toolbar containing all functionalities to open and save images,
+    to process filters, undo actions a.s.o.
+    
+    """
+    
     def __init__(self, master):
         
         toolFrame = Frame(master)
         toolFrame.grid()
         
         self.scrWidth = root.winfo_screenwidth() # width of the screen
-        self.scrHeight = root.winfo_screenheight()
+        self.scrHeight = root.winfo_screenheight() # height ---"----
+        self.imgWidth = None #imgage width
+        self.imgHeight = None # --"-- height
         self.tempDir = tempfile.TemporaryDirectory()
-        self.suffix = [] # suffix of file
+        self.suffix = [] # suffix of image file
+        self.x_true = IntVar() # used for
+        self.y_true = IntVar() # checkbuttons
+        self.fourierCheck = False
+        ######### Buttons #####################################################
         
-        self.x_resize = Entry(master)
-        self.x_resize.grid(row = 3, column = 0)
+        self.openButton = Button(toolFrame,\
+         text = "open", command = self.openImg, width = "13")
+        self.openButton.grid(row = 0, column = 0, sticky = None)
         
-        self.y_resize = Entry(master)
-        self.y_resize.grid(row = 4, column = 0)
-        
-        self.openButton = Button(toolFrame, text = "open", command = self.openImg, width = "13")
-        self.openButton.grid(row = 0, column = 0, sticky = "w")
-        
-        self.saveButton = Button(toolFrame, text = "save as", command = self.saveImg, width = "13")
-        #self.saveButton.grid(row = 0, column = 0)
-        
-        self.resizeButton = Button(toolFrame, text = "resize", command = self.resizeImg, width = "13")
-        self.resizeButton.grid(row = 1, column = 0, sticky = "w")
-        
-        self.test = Button(toolFrame, text = "Change frame size", command = self.chFrame, width = "13")
-        self.test.grid(row = 2, column = 0, sticky = "w")
+        self.saveButton = Button(toolFrame,\
+         text = "save as", command = self.saveImg, width = "13")
         
         
+        self.resizeButton = Button(toolFrame,\
+         text = "Scale", command = self.resizeImg, width = "13")
+        self.resizeButton.grid(row = 1, column = 0, sticky = None)
         
-        """
+        self.undoButton = Button(toolFrame,\
+         text = "Undo Action", command = self.undo, width = "13")
+        self.undoButton.grid(row = 5, column = 0, sticky = None)
+        
+        self.gaussButton = Button(toolFrame,\
+         text = "Gaussian filter", command = self.gauss, width = "13")
+        self.gaussButton.grid(row = 6, column = 0, sticky = None)
+###############################################################################        
+        self.flipVButton = Button(toolFrame,\
+         text = "Flip (vertical)", command = self.flip_vertical, width = "13")
+        self.flipVButton.grid(row = 7, column = 0, sticky = None)
+        
+        self.flipHButton = Button(toolFrame,\
+         text = "Flip (horizontal)", command = self.flip_horizon, width = "13")
+        self.flipHButton.grid(row = 8, column = 0, sticky = None)
+        
+        self.invertButton = Button(toolFrame,\
+         text = "Invert", command = self.invert, width = "13")
+        self.invertButton.grid(row = 9, column = 0, sticky = None)
+        
+        self.uniformButton = Button(toolFrame,\
+         text = "Uniform", command = self.uniform, width = "13")
+        self.uniformButton.grid(row = 10, column = 0, sticky = None)
+        
+        self.prewittButton = Button(toolFrame,\
+         text = "Prewitt", command = self.prewitt, width = "13")
+        self.prewittButton.grid(row = 11, column = 0, sticky = None)
+        
+        self.sobelButton = Button(toolFrame,\
+         text = "Sobel", command = self.sobel, width = "13")
+        self.sobelButton.grid(row = 12, column = 0, sticky = None)
+        
+        self.thresholdButton = Button(toolFrame,\
+         text = "Threshold", command = self.threshold, width = "13")
+        self.thresholdButton.grid(row = 13, column = 0, sticky = None)
+        
+        self.normalizeButton = Button(toolFrame,\
+         text = "Normalize", command = self.normalize, width = "13")
+        self.normalizeButton.grid(row = 14, column = 0, sticky = None)
+        
+        self.greyscaleButton = Button(toolFrame,\
+         text = "To Greyscale", command = self.greyscale, width = "13")
+        self.greyscaleButton.grid(row = 15, column = 0, sticky = None)
+        
+        self.fourierButton = Button(toolFrame,\
+         text = "Fourier", command = self.fourier, width = "13")
+        self.fourierButton.grid(row = 16, column = 0, sticky = None)
+        
+        self.histoButton = Button(toolFrame,\
+         text = "Histogram", command = self.histo, width = "13")
+        self.histoButton.grid(row = 17, column = 0, sticky = None)
+        
+        self.plotButton = Button(toolFrame,\
+         text = "plot comparison", command = self.plot, width = "13")
+        self.plotButton.grid(row = 20, column = 0, sticky = None)
+        
+        
+        ######------- Labels, entry widgets, checkbuttons -------##############
+        self.x_resize = Entry(toolFrame, width = "5")
+        self.x_resize.grid(row = 2, column = 0, sticky = "w")
+        
+        self.y_resize = Entry(toolFrame, width = "5")
+        self.y_resize.grid(row = 2, column = 0)
+        
+        self.ratio_x = Checkbutton(toolFrame,\
+         variable = self.x_true).grid(row = 4, column = 0, sticky = "w")
+        self.ratio_y = Checkbutton(toolFrame,\
+         variable = self.y_true).grid(row = 4, column = 0)
+          
         self.resizeLabelX = Label(toolFrame, text = "x ")
-        self.resizeLabelX.grid()
+        self.resizeLabelX.grid(row = 3, column = 0, sticky = "w")
         
         self.resizeLabelY = Label(toolFrame, text = "y ")       
-        self.resizeLabelY.grid()
+        self.resizeLabelY.grid(row = 3, column = 0)
         
-        """
+        self.histBinsLabel = Label(toolFrame, text = "bins")
+        self.histBinsLabel.grid(row = 18, column = 0, sticky = "w")
         
-        self.undoButton = Button(toolFrame, text = "Undo Action", command = self.undo, width = "13")
-        self.undoButton.grid(row = 4, column = 0, sticky = "w")
-        # how some filter button could look like
-        self.gaussButton = Button(toolFrame, text = "Gaussian filter", command = self.gauss, width = "13")
-        self.gaussButton.grid(row = 5, column = 0, sticky = "w")
+        self.histRangeLabel = Label(toolFrame, text = "range")
+        self.histRangeLabel.grid(row = 19, column = 0, sticky = "w")
         
-        self.flipVButton = Button(toolFrame, text = "Flip (vertical)", command = self.flip_vertical, width = "13")
-        self.flipVButton.grid(row = 7, column = 0, sticky = "w")
+        self.bins = Entry(toolFrame, width = "5")
+        self.bins.insert(END, "256")
+        self.bins.grid(row = 18, column = 0)
         
-        self.flipHButton = Button(toolFrame, text = "Flip (horizontal)", command = self.flip_horizon, width = "13")
-        self.flipHButton.grid(row = 8, column = 0, sticky = "w")
+        self.rangeFrom = Entry(toolFrame, width = "5")
+        self.rangeFrom.insert(END, "0")
+        self.rangeFrom.grid(row = 19, column = 0)
         
-        self.invertButton = Button(toolFrame, text = "Invert", command = self.invert, width = "13")
-        self.invertButton.grid(row = 9, column = 0, sticky = "w")
+        self.rangeTo = Entry(toolFrame, width = "5")
+        self.rangeTo.insert(END, "256")
+        self.rangeTo.grid(row = 19, column = 0, sticky = "e")
         
-        self.uniformButton = Button(toolFrame, text = "Uniform", command = self.uniform, width = "13")
-        self.uniformButton.grid(row = 10, column = 0, sticky = "w")
-        
-        self.prewittButton = Button(toolFrame, text = "Prewitt", command = self.prewitt, width = "13") # prewitt !1!!!1!
-        self.prewittButton.grid(row = 11, column = 0, sticky = "w")
-        
-        self.sobelButton = Button(toolFrame, text = "Sobel", command = self.sobel, width = "13")
-        self.sobelButton.grid(row = 12, column = 0, sticky = "w")
-        
-        self.thresholdButton = Button(toolFrame, text = "Threshold", command = self.threshold, width = "13")
-        self.thresholdButton.grid(row = 13, column = 0, sticky = "w")
-        
-        self.normalizeButton = Button(toolFrame, text = "Normalize", command = self.normalize, width = "13")
-        self.normalizeButton.grid(row = 14, column = 0, sticky = "w")
-        
-        self.greyscaleButton = Button(toolFrame, text = "To Greyscale", command = self.greyscale, width = "13")
-        self.greyscaleButton.grid(row = 15, column = 0, sticky = "w")
-        
-        self.fourierButton = Button(toolFrame, text = "Fourier", command = self.fourier, width = "13")
-        self.fourierButton.grid(row = 16, column = 0, sticky = "w")
-        
-        self.plotButton = Button(toolFrame, text = "plot comparison", command = self.plot, width = "13")
-        self.plotButton.grid(row = 17, column = 0, sticky = "w")
-    
     def plot(self):
     
         sk.io.imshow(pseudoStack.stack[0])
         sk.io.show()
     
-    def fourier(self):
+    def histo(self):
     
+        bins = int(self.bins.get())
+        print(bins)
+        minR = int(self.rangeFrom.get())
+        maxR = int(self.rangeTo.get())
+        print(minR, maxR)
+        
+        histogram = ndimage.histogram(pseudoStack.stack[0], 0.0, 1.0, 10)
+        plt.hist(pseudoStack.stack[0].ravel(), bins, [minR, maxR])
+        plt.show()
+        #pseudoStack.stack.insert(0, histogram)
+        #self.refresh()
+    
+    def fourier(self):
+        self.fourierCheck = True
         name = sk.imgED(pseudoStack.stack[0])
         name.fourier_transformation()
         self.refresh()
@@ -169,12 +234,25 @@ class Toolbar():
     # the filter button example       
     def resizeImg(self):
         
+###############################################################################        
         x = int(self.x_resize.get())
         y = int(self.y_resize.get())
-        imgResized = misc.imresize(pseudoStack.stack[0], (x,y), interp = 'cubic')
-        pseudoStack.stack.insert(0, imgResized)
+        if self.x_true.get() == True and self.y_true.get() == False:
+            percentX = int(self.imgWidth * (x / 100))
+            imgResized = misc.imresize(pseudoStack.stack[0],\
+             int(percentX), interp = 'bilinear')    
+            pseudoStack.stack.insert(0, imgResized)
+        elif self.x_true.get() == False and self.y_true.get() == True:
+            percentY = int(self.imgWidth * (x / 100))
+            imgResized = misc.imresize(pseudoStack.stack[0],\
+             int(percentY), interp = 'bilinear')
+            pseudoStack.stack.insert(0, imgResized)
+        else:
+            imgResized = misc.imresize(pseudoStack.stack[0],\
+             (x,y), interp = 'bilinear') 
+            pseudoStack.stack.insert(0, imgResized)
         
-        self.chFrame()
+        #self.chFrame()
         self.refresh()
         
     
@@ -192,22 +270,25 @@ class Toolbar():
             pass
         else:
             misc.imsave(path, pseudoStack.stack[0])
-        
+###############################################################################        
     def openImg(self):
         
-        
+       
         imgPath = filedialog.askopenfile()
         self.suffix = imgPath.name.split('.')
         self.suffix = self.suffix.pop()
         asArray = ndimage.imread(imgPath.name)
         pseudoStack.stack.insert(0, asArray)
         asPIL = PIL.Image.fromarray(asArray)
-        imgWidth = asPIL.size[0]
-        imgHeight = asPIL.size[1]
-        imgWinPos = "+" + str(int(self.scrWidth / 2) - int(imgWidth / 2)) + "+" + str(int(self.scrHeight / 2) - int(imgHeight / 2))
+        self.imgWidth = asPIL.size[0]
+        self.imgHeight = asPIL.size[1]
+        imgWinPos = "+" + str(int(self.scrWidth / 2) - int(self.imgWidth / 2))\
+         + "+" + str(int(self.scrHeight / 2) - int(self.imgHeight / 2))
+        
         self.imageWindow = Toplevel()
+        self.imageWindow.title("..." + imgPath.name[-10:])
         self.imageWindow.protocol("WM_DELETE_WINDOW", start.switchBtn)
-        self.saveButton.grid(row = 0, column = 0, sticky = "w")
+        self.saveButton.grid(row = 0, column = 0, sticky = None)
         self.imageWindow.geometry(imgWinPos)
         forTk = ImageTk.PhotoImage(asPIL)
         self.image = Label(self.imageWindow, image = forTk)
@@ -216,6 +297,7 @@ class Toolbar():
              
    
     def undo(self):
+        print(len(pseudoStack.stack))
         
         if len(pseudoStack.stack) == 1:
             pass
@@ -226,16 +308,25 @@ class Toolbar():
         
     def refresh(self):
         forTk = self.convert()
-        print(forTk)
         self.image.configure(image = forTk)
         self.image.grid()
         self.imageWindow.root.update()
     
     def convert(self):
-        misc.imsave(self.tempDir.name + "temp." + self.suffix, pseudoStack.stack[0]) 
-        lame = misc.imread(self.tempDir.name + "temp." + self.suffix)
+        
+        if self.fourierCheck == True:
+            io.imsave(self.tempDir.name\
+         + "temp." + self.suffix, pseudoStack.stack[0]) 
+            self.fourierCheck = False
+        else:
+            misc.imsave(self.tempDir.name\
+         + "temp." + self.suffix, pseudoStack.stack[0]) 
+        
+        lame = io.imread(self.tempDir.name + "temp." + self.suffix)
+        #pseudoStack.stack.insert(0, lame)
         asPIL = PIL.Image.fromarray(lame)
         forTk = ImageTk.PhotoImage(asPIL)
+        
         return forTk
     
     def switchBtn(self):
@@ -247,7 +338,7 @@ class Toolbar():
 
 root = Tk()
 start = Toolbar(root)
-w = 145 # width for the Tk root
+w = 131 # width for the Tk root
 h = 600 # height for the Tk root
 
 
